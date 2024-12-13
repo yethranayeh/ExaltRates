fc65877f4d2b426bf9dbab86a7a2b8934ee9934f
Alper Aktaş <aktasalper99@gmail.com>
2024-12-13 18:33:53 +0300

    add disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import "./assets/fonts/fontin/Fontin-Regular.ttf";
 import { DatabaseContextProvider } from "./context/DatabaseContextProvider";
 import { Header } from "./components/Header";
 import { CurrencyConverter } from "./components/CurrencyConverter";
+import { Footer } from "./components/Footer";
 import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
 	return (
-		<div className='flex flex-col gap-4'>
+		<div className='flex flex-col gap-4 min-h-screen'>
 			<Header />
-			<ErrorBoundary>
-				<DatabaseContextProvider>
-					<CurrencyConverter />
-				</DatabaseContextProvider>
-			</ErrorBoundary>
+			<div className='flex-1'>
+				<ErrorBoundary>
+					<DatabaseContextProvider>
+						<CurrencyConverter />
+					</DatabaseContextProvider>
+				</ErrorBoundary>
+			</div>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+	return (
+		<footer className='p-2 text-center text-sm text-primary-dark'>
+			This website is not affiliated with, maintained, endorsed or sponsored by Grinding Gear Games or any of its
+			affiliates.
+		</footer>
+	);
+}
