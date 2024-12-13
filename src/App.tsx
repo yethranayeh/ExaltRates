25f1f177466ee46292e2f3d87b8303b8f843f59c
Alper Aktaş <aktasalper99@gmail.com>
2024-12-13 14:27:49 +0300

    add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./index.css";
 import { DatabaseContextProvider } from "./context/DatabaseContextProvider";
 import { CurrencyConverter } from "./components/CurrencyConverter";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
 	return (
 		<div>
-			<div>
+			<ErrorBoundary>
 				<DatabaseContextProvider>
 					<CurrencyConverter />
 				</DatabaseContextProvider>
-			</div>
+			</ErrorBoundary>
 		</div>
 	);
 }
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,58 @@
+// https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/error_boundaries/
+import { Component, ErrorInfo, ReactNode } from "react";
+import { Alert, AlertDescription, AlertTitle } from "./shadcn/Alert";
+import { AlertCircle } from "lucide-react";
+
+interface Props {
+	children?: ReactNode;
+}
+
+interface State {
+	hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+	public state: State = {
+		hasError: false
+	};
+
+	public static getDerivedStateFromError(_: Error): State {
+		// Update state so the next render will show the fallback UI.
+		return { hasError: true };
+	}
+
+	public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+		console.error("EXALT_RATES_ERR:", error, errorInfo);
+	}
+
+	public render() {
+		if (this.state.hasError) {
+			return (
+				<div className='w-screen h-screen grid place-items-center'>
+					<Alert variant='destructive' className='w-full max-w-[450px]'>
+						<AlertCircle className='h-4 w-4' />
+						<AlertTitle>Error</AlertTitle>
+						<AlertDescription>
+							<p>Something went wrong while preparing the currency conversion calculator. Please check back later.</p>
+							<p className='mt-2'>
+								You can also{" "}
+								<a href='https://github.com/yethranayeh/ExaltRates/issues' target='_blank' className='underline'>
+									submit this issue
+								</a>{" "}
+								with error details.
+							</p>
+							<p className='opacity-50'>
+								The details of the error (starting with "<span className='font-bold'>EXALT_RATES_ERR</span>") can be
+								viewed in the developer console.
+							</p>
+						</AlertDescription>
+					</Alert>
+				</div>
+			);
+		}
+
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundary;
